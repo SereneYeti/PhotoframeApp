8bebe33ad3e3bdb3c753045f87b634ee00a02d1a
James D Vieyra <james@vieyra.co.za>
2022-05-24 14:29:41 +0200

    Updated Colour Changing
    Colour changing now uses the integrated colourWell component for colour picking.

--- PhotoframeApp/ViewController.swift
+++ PhotoframeApp/ViewController.swift
@@ -9,18 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var ColourOptions: Array<UIColor> = [UIColor.gray, UIColor.red,UIColor.green,UIColor.blue,UIColor.orange,UIColor.yellow,UIColor.cyan,UIColor.purple,UIColor.systemPink,UIColor.systemBackground];
-    //var images:Array<String> = ["IMG_00", "IMG_01", "IMG_02", "IMG_03", "IMG_04", "IMG_05", "IMG_06", "IMG_07"];
+    let colourWell: UIColorWell = {
+        let colourWell = UIColorWell()
+        colourWell.supportsAlpha = true
+        colourWell.selectedColor = .systemRed
+        colourWell.title = "Background Colour Picker"
+        return colourWell
+    }()
+    let colourWell2: UIColorWell = {
+        let colourWell = UIColorWell()
+        colourWell.supportsAlpha = true
+        colourWell.selectedColor = .systemBlue
+        colourWell.title = "Frame Colour Picker"
+        return colourWell
+    }()
+    
     var pictures = [String]()
     var currentImg:Int = 0;
     
     
     var frameCount:Int = 0;
-    var backgroundCount:Int = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Colour Well Setup
+        view.backgroundColor = colourWell.selectedColor;
+        FrameView.backgroundColor = colourWell2.selectedColor;
+        view.addSubview(colourWell)
+        
+        colourWell.addTarget(self, action: #selector(backgroundColourChanged), for: .valueChanged)
+        
+        view.addSubview(colourWell2)
+        
+        colourWell2.addTarget(self, action: #selector(frameColourChanged), for: .valueChanged)
+        
+        //Find Images
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path);
@@ -32,10 +56,7 @@ class ViewController: UIViewController {
                 pictures.append(item);
             }
         }
-        
-        frameCount = 0;
-        backgroundCount = 0;
-        
+       
         currentImg = 0;
         lblImageName.text = pictures[currentImg]
         ivPhotoContainer.image = UIImage (imageLiteralResourceName: pictures[currentImg]);
@@ -44,31 +65,20 @@ class ViewController: UIViewController {
         print(pictures);
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        colourWell.frame = CGRect(x: view.safeAreaInsets.left+10, y: view.safeAreaInsets.bottom,
+                                  width: 50, height: 50)
+        colourWell2.frame = CGRect(x: view.safeAreaInsets.left+10, y: view.safeAreaInsets.bottom+50,
+                                  width: 50, height: 50)
+    }
+    
     @IBOutlet var BackgroundView: UIView!
     @IBOutlet weak var ivPhotoContainer: UIImageView!
     @IBOutlet weak var FrameView: UIView!
     @IBOutlet weak var lblImageName: UILabel!
     
-    @IBAction func ChangeFrameColour(_ sender: Any) {
-        FrameView.backgroundColor = ColourOptions[frameCount];
-        if(frameCount < ColourOptions.count-1){
-            frameCount+=1;
-        }
-        else
-        {
-            frameCount = 0;
-        }
-    }
-    @IBAction func ChangeBackgroundColour(_ sender: Any) {
-        BackgroundView.backgroundColor = ColourOptions[backgroundCount];
-        if(backgroundCount < ColourOptions.count-1){
-            backgroundCount+=1;
-        }
-        else
-        {
-            backgroundCount = 0;
-        }
-    }
+    
     @IBAction func NextPhoto(_ sender: Any) {
         if(currentImg < pictures.count-1){
             currentImg += 1;
@@ -90,6 +100,15 @@ class ViewController: UIViewController {
         print("ITEM NUMBER: " + String(currentImg));
         lblImageName.text = pictures[currentImg]
         ivPhotoContainer.image = UIImage (imageLiteralResourceName: pictures[currentImg]);
+    }
+    
+    @objc private func backgroundColourChanged(){
+        view.backgroundColor = colourWell.selectedColor
+        
+    }
+    @objc private func frameColourChanged(){
+        FrameView.backgroundColor = colourWell2.selectedColor
+        
     }
 }
 
